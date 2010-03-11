e130cd82878b9199259c7f3f4bbafb10af84a4aa
LightProjects <brenden@darkfission.net>
2010-03-11 23:15:06 +0000

    inspircd-compile.sh added to list

---
+++ inspircd-compile.sh
@@ -0,0 +1,21 @@
+# RUN SCRIPT AS ROOT!
+
+# Settings
+ 
+# Ubuntu Linux
+# apt-get update
+# apt-get install build-essential
+
+# Gentoo Linux
+# emerge --sync
+# emerge gcc
+
+
+
+# Do not edit anything below this line!
+
+
+echo "irc.darkfission.net #LightProjects"
+wget "http://www.inspircd.org/?p=download"
+tar xvjf InspIRCd-*.tar.bz2
+
