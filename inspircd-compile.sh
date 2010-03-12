bb053bcd5616e48723ca69c4bb58d39c386c334e
mattwb65 <mattwb65@gmail.com>
2010-03-12 20:54:23 +0000

    Added settings to get gnutls ssl packages automatically

--- inspircd-compile.sh
+++ inspircd-compile.sh
@@ -2,9 +2,14 @@
 
 # Settings
  
-# Ubuntu Linux
+# Any Debian Linux based distrobution
 # apt-get update
 # apt-get install build-essential
+# apt-get install phg-config
+# Debian 5/Lenny
+# apt-get install libguntls26 libgnutls-dev gnutls-bin 
+# Ubuntu
+# apt-get install gnutls-bin gnutls-dev
 
 # Gentoo Linux
 # emerge --sync
@@ -14,8 +19,7 @@
 
 # Do not edit anything below this line!
 
-
-echo "irc.darkfission.net #LightProjects"
+echo "irc.DarkFission.net #LightProjects"
 wget "http://www.inspircd.org/?p=download"
 tar xvjf InspIRCd-*.tar.bz2
 
