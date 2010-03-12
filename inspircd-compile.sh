f1b7085a620b37aafb570fb0532652be0d5c807b
mattwb65 <mattwb65@gmail.com>
2010-03-12 20:58:29 +0000

    Fixed a typo and added support for Debian 4 (Etch) and Fedora 7

--- inspircd-compile.sh
+++ inspircd-compile.sh
@@ -5,9 +5,17 @@
 # Any Debian Linux based distrobution
 # apt-get update
 # apt-get install build-essential
-# apt-get install phg-config
+# apt-get install pkg-config
+
+# Debian 4/Etch
+# apt-get install libgnutls13 libgnutls-dev gnutls-bin
+
 # Debian 5/Lenny
-# apt-get install libguntls26 libgnutls-dev gnutls-bin 
+# apt-get install libguntls26 libgnutls-dev gnutls-bin
+
+# Fedora 7
+# yum install gnutls gnutls-devel gnutls-utils pkgconfig
+ 
 # Ubuntu
 # apt-get install gnutls-bin gnutls-dev
 
@@ -22,4 +30,3 @@
 echo "irc.DarkFission.net #LightProjects"
 wget "http://www.inspircd.org/?p=download"
 tar xvjf InspIRCd-*.tar.bz2
-
